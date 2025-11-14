fdd9da7a0d27a1f6c0cf88b5defaff4a4f962508
shaked cohen <shakedcohen@shakeds-MacBook-Air.local>
2025-11-14 15:54:47 +0200

    first try for Algebra

--- Algebra.java
+++ Algebra.java
@@ -5,63 +5,100 @@
 
 public class Algebra {
 	public static void main(String args[]) {
-	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
+		// Tests some of the operations
+		System.out.println(plus(2, 3)); // 2 + 3
+		System.out.println(minus(7, 2)); // 7 - 2
+		System.out.println(minus(2, 7)); // 2 - 7
+		System.out.println(times(3, 4)); // 3 * 4
+		System.out.println(plus(2, times(4, 2))); // 2 + 4 * 2
+		System.out.println(pow(5, 3)); // 5^3
+		System.out.println(pow(3, 5)); // 3^5
+		System.out.println(div(12, 3)); // 12 / 3
+		System.out.println(div(5, 5)); // 5 / 5
+		System.out.println(div(25, 7)); // 25 / 7
+		System.out.println(mod(25, 7)+"->4"); // 25 % 7
+		System.out.println(mod(120, 6)+"->0"); // 120 % 6
+		System.out.println(sqrt(36)+"----->6");
 		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
-	}  
+		System.out.println(sqrt(76123));
+		System.out.println(sqrt(8));
+	}
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = x1;
+		while (x2 > 0) {
+			result++;
+			x2--;
+		}
+		return result;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = x1;
+		while (x2 > 0) {
+			result--;
+			x2--;
+		}
+		return result;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 0;
+		while (x2 > 0) {
+			result = plus(result, x1);
+			x2--;
+		}
+		return result;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		if (n == 0)
+			return 1;
+		int result = 1;
+
+		while (n > 0) {
+			result = times(result, x); 
+			n--; 
+		}
+		return result;
 	}
 
-	// Returns the integer part of x1 / x2 
+	// Returns the integer part of x1 / x2
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = x1;
+		int counter=0;
+		while (x2 <=result) {
+			result = minus(result, x2);
+			counter++;
+		}
+		return counter;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}	
-
-	// Returns the integer part of sqrt(x) 
+	int howMuch=div(x1, x2);
+	int times= times(howMuch, x2);
+	int result = minus(x1, times);
+	
+		return result;
+	}
+	
+	
+	// Returns the integer part of sqrt(x)
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
-	}	  	  
+		int counter=0;
+		int pow=0;
+		while(pow<x){
+			counter++;
+			pow=times(counter, counter);
+		}
+		if (pow(counter, 2)==x)
+			return counter;
+		else counter--;	
+		return counter; 
+	}
 }
